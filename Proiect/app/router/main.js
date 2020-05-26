fb12a08e2251583eb5c43027724c90c4ab020032
adamcristi <adamcristi98@yahoo.com>
2020-05-27 00:06:30 +0300

    Added proiect

---
+++ Proiect/app/router/main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const datasets_router = require('./datasets');
+const models_router = require('./models');
+
+router.get('/', (req, res) => {
+    res.render('home', {title: 'Cloud ML'});
+});
+
+router.get('/index.html', (req, res) => {
+    res.render('index', {title: 'Cloud ML'});
+});
+
+router.use('/datasets', datasets_router);
+router.use('/models', models_router);
+
+module.exports = router;
